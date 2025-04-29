39f86a97d04868274d8bae8cba4abe300eccdc46
Nguyen Hoang Vu <88179705+vu0cay@users.noreply.github.com>
2025-04-29 22:15:06 +0700

    remove redundant

--- maven_build.bat
+++
@@ -1,5 +0,0 @@
-docker build -t vu0cay/ctu-imdf-platform:latest .
-docker push vu0cay/ctu-imdf-platform:latest 
-@REM docker-compose down                   
-docker pull vu0cay/ctu-imdf-platform:latest 
-docker-compose up -d
